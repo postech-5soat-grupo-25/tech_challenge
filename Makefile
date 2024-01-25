8e472e287ed28349b0eaea269fffa08c787e2d12
Bruno Mafra <brunomp2714@gmail.com>
2024-01-24 21:54:42 -0300

    criando primeira migration

--- Makefile
+++ Makefile
@@ -3,6 +3,25 @@ all: up
 
 .PHONY: build
 build:
+	docker compose up db -d
+	sleep 10
+	docker ps
+	docker cp ./migrations/0001_create_table.sql tech_challenge-db-1:/0001_create_table.sql
+	docker compose exec db psql -U postgres -d postgres -a -f 0001_create_table.sql
+
+.PHONY: run
+run:
+	docker compose up --remove-orphans
+
+.PHONY: down
+down:
+	docker compose down
+
+.PHONY: all
+all: up
+
+.PHONY: build
+build:
 	docker compose up --build --remove-orphans
 
 .PHONY: run
---
+++ migrations/0001_create_table.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS usuario (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    senha VARCHAR(255),
+    cpf VARCHAR(255),
+    endereco VARCHAR(255),
+    data_criacao TIMESTAMP,
+    data_atualizacao TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS produto (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    foto VARCHAR(255),
+    descricao VARCHAR(255), 
+    categoria INT,
+    preco MONEY,
+    ingredientes VARCHAR(255)[],
+    data_criacao TIMESTAMP,
+    data_atualizacao TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS cliente (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    cpf VARCHAR(255),
+    data_criacao TIMESTAMP,
+    data_atualizacao TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS pedido (
+    id SERIAL PRIMARY KEY,
+    cliente INT,
+    lanche INT,
+    acompanhamento INT,
+    bebida INT,
+    pagamento VARCHAR(255),
+    status INT,
+    data_criacao TIMESTAMP,
+    data_atualizacao TIMESTAMP,
+    CONSTRAINT fk_cliente FOREIGN KEY (cliente) REFERENCES cliente(id),
+    CONSTRAINT fk_lanche FOREIGN KEY (lanche) REFERENCES produto(id),
+    CONSTRAINT fk_acompanhamento FOREIGN KEY (acompanhamento) REFERENCES produto(id),
+    CONSTRAINT fk_bebida FOREIGN KEY (bebida) REFERENCES produto(id)
+);
