6a2f629acff15ebf848b2c81108e2ef599514046
Bruno Mafra <brunomp2714@gmail.com>
2024-01-25 21:01:04 -0300

    atualizando tabela de usuario

--- migrations/0001_create_table.sql
+++ migrations/0001_create_table.sql
@@ -2,9 +2,10 @@ CREATE TABLE IF NOT EXISTS usuario (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255),
     email VARCHAR(255),
-    senha VARCHAR(255),
     cpf VARCHAR(255),
-    endereco VARCHAR(255),
+    senha VARCHAR(255),
+    tipo INT,
+    status INT, 
     data_criacao TIMESTAMP,
     data_atualizacao TIMESTAMP
 );
