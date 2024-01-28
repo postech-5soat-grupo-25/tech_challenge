d86c5a571f73bdc9e443c441e820cffdfccad1f2
Matheus Bachiste Lopes <matheusblopes10@gmail.com>
2024-01-28 20:29:50 -0300

    feat: update pedido controller

--- src/adapter/api/controllers/pedido_controller.rs
+++ src/adapter/api/controllers/pedido_controller.rs
@@ -10,6 +10,15 @@ use crate::adapter::api::request_guards::authentication_guard::AuthenticatedUser
 use crate::core::application::use_cases::pedidos_e_pagamentos_use_case::{PedidosEPagamentosUseCase, CreatePedidoInput};
 use crate::core::domain::entities::pedido::Pedido;
 
+
+#[openapi(tag = "Pedidos")]
+#[get("/pedido/<id>")]
+async fn get_pedido(pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>, id: usize, _logged_user_info: AuthenticatedUser) -> Result<Json<Pedido>, Status> {
+    let pedido = pedido_e_pagamentos_use_case.get_order_by_id(id).await?;
+    Ok(Json(pedido))
+}
+
+
 #[openapi(tag = "Pedidos")]
 #[post("/pedido", data="<pedido_input>")]
 async fn post_novo_pedido(
