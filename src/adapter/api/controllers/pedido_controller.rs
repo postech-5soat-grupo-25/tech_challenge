8d8e55513e21a22707f4eadb161c52a4e6de62a9
MatheusBLopes <matheusblopes10@gmail.com>
2024-01-29 13:48:15 -0300

    feat: update pedido controller, repository and use case

--- src/adapter/api/controllers/pedido_controller.rs
+++ src/adapter/api/controllers/pedido_controller.rs
@@ -12,28 +12,6 @@ use crate::core::domain::entities::pedido::Pedido;
 
 
 #[openapi(tag = "Pedidos")]
-#[get("/pedido/<id>")]
-async fn get_pedido(pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>, id: usize, _logged_user_info: AuthenticatedUser) -> Result<Json<Pedido>, Status> {
-    let pedido = pedido_e_pagamentos_use_case.get_order_by_id(id).await?;
-    Ok(Json(pedido))
-}
-
-#[openapi(tag = "Pedidos")]
-#[get("/pedido/acompanhamento/")]
-async fn get_acompanhamentos(pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>, id: usize, _logged_user_info: AuthenticatedUser) -> Result<Json<Vec<Produtos>>, Status> {
-    let acompanhamentos = pedido_e_pagamentos_use_case.lista_acompanhamentos().await?;
-    Ok(Json(acompanhamentos))
-}
-
-#[openapi(tag = "Pedidos")]
-#[get("/pedido/bebida/")]
-async fn get_bebidas(pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>, id: usize, _logged_user_info: AuthenticatedUser) -> Result<Json<Vec<Produtos>>, Status> {
-    let bebidas = pedido_e_pagamentos_use_case.lista_bebidas().await?;
-    Ok(Json(bebidas))
-}
-
-
-#[openapi(tag = "Pedidos")]
 #[post("/pedido", data="<pedido_input>")]
 async fn post_novo_pedido(
     pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>,
@@ -42,6 +20,51 @@ async fn post_novo_pedido(
 ) -> Result<Json<Pedido>, Status> {
     let novo_pedido = pedido_e_pagamentos_use_case.novo_pedido(pedido_input).await?;
     Ok(Json(novo_pedido))
+}
+
+#[openapi(tag = "Pedidos")]
+#[get("/pedido/<id>")]
+async fn get_pedido(pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>, id: usize, _logged_user_info: AuthenticatedUser) -> Result<Json<Pedido>, Status> {
+    let pedido = pedido_e_pagamentos_use_case.seleciona_pedido_por_id(id).await?;
+    Ok(Json(pedido))
+}
+
+#[openapi(tag = "Pedidos")]
+#[post("/pedido/acompanhamento/<pedido_id>/<acompanhamento_id>")]
+async fn post_acompanhamentos(
+    pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>, 
+    pedido_id: usize,
+    acompanhamento_id: usize,
+    _logged_user_info: AuthenticatedUser
+) -> Result<Json<Pedido>, Status> {
+    let pedido = pedido_e_pagamentos_use_case.adicionar_acompanhamento(pedido_id, acompanhamento_id).await?;
+    Ok(Json(pedido))
+}
+
+
+#[openapi(tag = "Pedidos")]
+#[post("/pedido/lanche/<pedido_id>/<lanche_id>")]
+async fn post_lanches(
+    pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>, 
+    pedido_id: usize,
+    lanche_id: usize,
+    _logged_user_info: AuthenticatedUser
+) -> Result<Json<Pedido>, Status> {
+    let pedido = pedido_e_pagamentos_use_case.adicionar_lanche(pedido_id, lanche_id).await?;
+    Ok(Json(pedido))
+}
+
+
+#[openapi(tag = "Pedidos")]
+#[post("/pedido/bebida/<pedido_id>/<bebida_id>")]
+async fn post_bebidas(
+    pedido_e_pagamentos_use_case: &State<PedidosEPagamentosUseCase>, 
+    pedido_id: usize,
+    bebida_id: usize,
+    _logged_user_info: AuthenticatedUser
+) -> Result<Json<Pedido>, Status> {
+    let pedido = pedido_e_pagamentos_use_case.adicionar_bebida(pedido_id, bebida_id).await?;
+    Ok(Json(pedido))
 }
 
 pub fn routes() -> Vec<rocket::Route> {
--- src/core/domain/repositories/pedido_repository.rs
+++ src/core/domain/repositories/pedido_repository.rs
@@ -12,13 +12,13 @@ pub trait PedidoRepository {
 
   async fn get_pedido_by_id(&self, pedido_id: usize) -> Result<Pedido, DomainError>;
 
-  async fn cadastrar_acompanhamento(&mut self, pedido_id: usize, acompanhamento: Produto) -> Result<Pedido, DomainError>;
+  async fn cadastrar_acompanhamento(&mut self, pedido_id: usize, acompanhamento_id: usize) -> Result<Pedido, DomainError>;
   
-  async fn cadastrar_bebida(&mut self, pedido_id: usize, bebida: Produto) -> Result<Pedido, DomainError>;
+  async fn cadastrar_bebida(&mut self, pedido_id: usize, bebida_id: usize) -> Result<Pedido, DomainError>;
 
   async fn cadastrar_pagamento(&mut self, pedido_id: usize, pagamento: String) -> Result<Pedido, DomainError>;
 
-  async fn cadastrar_lanche(&mut self, pedido_id: usize, lanche: Produto) -> Result<Pedido, DomainError>;
+  async fn cadastrar_lanche(&mut self, pedido_id: usize, lanche_id: usize) -> Result<Pedido, DomainError>;
 
   async fn update_pedido(&mut self, pedido_id: usize, pedido: Pedido) -> Result<Pedido, DomainError>;
 }
